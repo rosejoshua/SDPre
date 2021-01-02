898aa5869ea64f1fa1e0e6e2b5c1e4287c6d6e35
Joshua Rose <jlr007@protonmail.com>
2021-01-02 17:30:32 -0500

    complete Objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,8 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    String myVariable = "hot";
+    System.out.println(myVariable);
+    myVariable = "cold";
+    System.out.println(myVariable);
+  }
+}
