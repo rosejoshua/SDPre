4f677acb4a9771c5d6f314053d443a313a3ead8e
Joshua Rose <jlr007@protonmail.com>
2021-01-02 18:20:47 -0500

    complete Objective3Lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,14 @@
+public class Objective3Lab1 {
+  public static void main(String[] args) {
+    double num1, num2, num3, avg;
+
+    num1 = 38438.54;
+    num2 = -5.2;
+    num3 = 849200847.4;
+
+    avg = (num1 + num2 + num3)/3;
+
+    System.out.println("The average is: " + avg);
+
+  }
+}
