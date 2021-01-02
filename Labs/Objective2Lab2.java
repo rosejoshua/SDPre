842d6b4ebef8fa2a331c6de41559c2d0ea03ccc8
Joshua Rose <jlr007@protonmail.com>
2021-01-02 17:26:36 -0500

    complete Objective2Lab2.java

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,11 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Joshua Rose";
+    int age = 36;
+    String state = "Ohio";
+
+    System.out.println("Name: " + name + " " +
+     "Age: " + age + " " + "State: " + state);
+
+  }
+}
