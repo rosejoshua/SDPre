f6bb95ac4ee6ac87b3b91a7f664a9a0a0fe84ad5
Joshua Rose <jlr007@protonmail.com>
2021-01-04 15:49:07 -0500

    completeObjective7Lab5

---
+++ Labs/Objective7Lab5.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Objective7Lab5 {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    int selection;
+    boolean run = true;
+
+    while (run == true) {
+      System.out.println("_____Menus_____");
+      System.out.println("1: Say Hello");
+      System.out.println("2: List my favorite foods");
+      System.out.println("3: Exit");
+      System.out.println();
+
+      selection = input.nextInt();
+
+      if(selection == 1) {
+        System.out.println("Hello, human!");
+      }
+
+      else if(selection == 2) {
+        System.out.println("My favorite foods are electrons and silicone!");
+      }
+
+      else if(selection == 3) {
+        System.out.println("Goodbye!");
+        run = false;
+      }
+
+      else {
+        System.out.println("Invalid input, please try again");
+      }
+    }
+  }
+}
