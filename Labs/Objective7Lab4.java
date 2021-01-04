344b1de351e8486be81a056924ae4a35492cda54
Joshua Rose <jlr007@protonmail.com>
2021-01-04 14:45:09 -0500

    completeObjective7Lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,13 @@
+public class Objective7Lab4 {
+  public static void main(String[] args) {
+    int count = 1;
+    int sum = 0;
+
+    while(count < 21) {
+      sum += count;
+      count ++;
+    }
+
+    System.out.println(sum);
+  }
+}
